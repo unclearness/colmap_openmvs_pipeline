b8981e374a20e681c93340e86670b4053c5d7a59
unclearness <unclearness.dev@gmail.com>
2025-12-24 20:47:25 +0900

    Add colmap stereo stub file generation

---
+++ make_stub_colmap_stereo_cfg.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import argparse
+
+
+def main(images_dir: Path, stereo_dir: Path):
+    stereo_dir.mkdir(parents=True, exist_ok=True)
+    image_names = images_dir.glob("*.jpg")
+    image_names = sorted(image_names)
+
+    fusion_cfg_path = stereo_dir / "fusion.cfg"
+    with fusion_cfg_path.open("w") as f:
+        for image_path in image_names:
+            f.write(f"{image_path.name}\n")
+
+    patchmatch_cfg_path = stereo_dir / "patch-match.cfg"
+    with patchmatch_cfg_path.open("w") as f:
+        for image_path in image_names:
+            f.write(f"{image_path.name}\n")
+            f.write("__auto__, 20\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create stub COLMAP stereo config files."
+    )
+    parser.add_argument(
+        "images_dir", type=Path, help="Directory containing input images."
+    )
+    parser.add_argument(
+        "stereo_dir", type=Path, help="Directory to save stereo config files."
+    )
+    args = parser.parse_args()
+
+    main(args.images_dir, args.stereo_dir)
